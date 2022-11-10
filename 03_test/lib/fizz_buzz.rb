ceaff5943a9cf2b7f080bcef70bac37548c343d1
pro-soichiro <dr.1987626@gmail.com>
2022-11-10 11:10:04 +0900

    finish 3 section

---
+++ 03_test/lib/fizz_buzz.rb
@@ -0,0 +1,20 @@
+def fizz_buzz(num)
+  if num % 15 == 0
+    "Fizz Buzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0
+    "Fizz"
+  else
+    num.to_s
+  end
+end
+
+fizz_buzz(1) #=> 1
+fizz_buzz(2) #=> 2
+fizz_buzz(3) #=> Fizz
+fizz_buzz(4) #=> 4
+fizz_buzz(5) #=> Buzz
+fizz_buzz(6) #=> Fizz
+fizz_buzz(15) #=> Fizz Buzz
+
